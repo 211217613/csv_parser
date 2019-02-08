1bd5c7e8b6ea21a480ebb6ab9b9af8652f6c7f0a
211217613 <rubenavazquez@gmail.com>
2019-02-07 23:11:57 -0500

    Fix linting errors.

--- main.py
+++ main.py
@@ -4,16 +4,29 @@ import re
 words = ['Payment', 'Adjustment', 'Return']
 more_words = 'RAPPI'
 
+
 def main():
     try:
         with open('./Chase0218_Activity20170126_20190126_20190126.csv', 'r') as csv_file, open('./not_sales.csv', 'w') as not_sales, open('./sales.csv', 'w') as sales:
             costs = csv.reader(csv_file, delimiter=',')
             x = csv.writer(not_sales)
             y = csv.writer(sales)
-            y.writerow(['Transaction Date', 'Post Date', 'Description', 'Category', 'Type', 'Amount'])
-            x.writerow(['Transaction Date', 'Post Date', 'Description', 'Category', 'Type', 'Amount'])
-
-
+            y.writerow(
+                       ['Transaction Date',
+                        'Post Date',
+                        'Description',
+                        'Category',
+                        'Type',
+                        'Amount']
+            )
+            x.writerow(
+                       ['Transaction Date',
+                        'Post Date',
+                        'Description',
+                        'Category',
+                        'Type',
+                        'Amount']
+             )
 
             for line in costs:
                 if 'Sale' in line:
@@ -21,7 +34,6 @@ def main():
                     print(string.strip())
                     print(re.search('^[+-]', string))
                     # x.writerow(string.strip())
-
 
                 else:
                     y.writerow(line[-2:-1])
