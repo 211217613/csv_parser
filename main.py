958c3b702308fea57616d062a33ab380309ec8a6
Ruben Vazquez <rubenavazquez@gmail.com>
2020-01-07 10:29:34 -0800

    Add parsing of file via command line args

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
 import csv
 import re
+import sys
 
 words = ['Payment', 'Adjustment', 'Return']
-more_words = 'RAPPI'
+pattern = 'RAPPI'
 
+if len(sys.argv) != 2:
+    print("Usage: main.py path-to-csv-file")
+    exit(1)
+
+try:
+    with open(sys.argv[1], 'r') as csv_file:
+        transactions = csv.reader(csv_file, delimiter=',')
+        for row in transactions:
+            row = ','.join(row)
+            match = re.search(pattern, row)
+            if match:
+                print(row)
+except FileNotFoundError as file_not_found:
+    print("File not found")
+except IOError as io_error:
+    print("ioerror")
 
 def main():
-    try:
-        with open('./Chase0218_Activity20170126_20190126_20190126.csv', 'r') as csv_file, open('./not_sales.csv', 'w') as not_sales, open('./sales.csv', 'w') as sales:
-            costs = csv.reader(csv_file, delimiter=',')
-            x = csv.writer(not_sales)
-            y = csv.writer(sales)
-            y.writerow(
-                       ['Transaction Date',
-                        'Post Date',
-                        'Description',
-                        'Category',
-                        'Type',
-                        'Amount']
-            )
-            x.writerow(
-                       ['Transaction Date',
-                        'Post Date',
-                        'Description',
-                        'Category',
-                        'Type',
-                        'Amount']
-             )
-
-            for line in costs:
-                if 'Sale' in line:
-                    string = r' '.join(line)
-                    print(string.strip())
-                    print(re.search('^[+-]', string))
-                    # x.writerow(string.strip())
-
-                else:
-                    y.writerow(line[-2:-1])
-                    # print(line[:-1])
-
-    except IOError as e:
-        print("Can't open file...")
-
+    print('Main')
 
 if __name__ == '__main__':
     main()
