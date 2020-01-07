995056adf2d40fdd48f5fbf49516af08ee05ceaa
Ruben Vazquez <rubenavazquez@gmail.com>
2020-01-07 10:32:08 -0800

    black

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import csv
 import re
 import sys
 
-words = ['Payment', 'Adjustment', 'Return']
-pattern = 'RAPPI'
+words = ["Payment", "Adjustment", "Return"]
+pattern = "RAPPI"
 
 if len(sys.argv) != 2:
     print("Usage: main.py path-to-csv-file")
     exit(1)
 
 try:
-    with open(sys.argv[1], 'r') as csv_file:
-        transactions = csv.reader(csv_file, delimiter=',')
+    with open(sys.argv[1], "r") as csv_file:
+        transactions = csv.reader(csv_file, delimiter=",")
         for row in transactions:
-            row = ','.join(row)
+            row = ",".join(row)
             match = re.search(pattern, row)
             if match:
                 print(row)
@@ -22,8 +22,10 @@ except FileNotFoundError as file_not_found:
 except IOError as io_error:
     print("ioerror")
 
-def main():
-    print('Main')
 
-if __name__ == '__main__':
+def main():
+    print("Main")
+
+
+if __name__ == "__main__":
     main()
